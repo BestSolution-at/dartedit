ddbbcc211d5c3ed11ae78b70cc3272caf86b9905
Tom Schindl <tom.schindl@bestsolution.at>
2015-07-22 10:09:08 +0200

    reuse theme definition

--- bundles/at.bestsolution.dart.editor/src/at/bestsolution/dart/editor/style/DartStylesheet.java
+++ bundles/at.bestsolution.dart.editor/src/at/bestsolution/dart/editor/style/DartStylesheet.java
@@ -1,26 +1,10 @@
 package at.bestsolution.dart.editor.style;
 
-import java.net.URL;
-
+import org.eclipse.fx.code.editor.fx.themes.EditorStylesheet;
 import org.eclipse.fx.ui.services.theme.Stylesheet;
-import org.eclipse.fx.ui.services.theme.Theme;
 import org.osgi.service.component.annotations.Component;
 
-@Component
-public class DartStylesheet implements Stylesheet {
-	@Override
-	public boolean appliesToTheme(Theme t) {
-		return true;
-	}
+@Component(service=Stylesheet.class)
+public class DartStylesheet extends EditorStylesheet {
 
-	@Override
-	public URL getURL(Theme t) {
-		URL url = null;
-		if( "dark".equals(t.getId()) ) {
-			url = getClass().getClassLoader().getResource("css/dark-highlight.css");
-		} else {
-			url = getClass().getClassLoader().getResource("css/highlight.css");
-		}
-		return url;
-	}
 }
