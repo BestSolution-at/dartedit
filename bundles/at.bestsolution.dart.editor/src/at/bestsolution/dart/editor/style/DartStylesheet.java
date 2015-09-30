0e4bb0fab627343bc95578ad8e6dc3d01b92722e
Tom Schindl <tom.schindl@bestsolution.at>
2015-09-30 13:20:36 +0200

    fix stylesheet integration

--- bundles/at.bestsolution.dart.editor/src/at/bestsolution/dart/editor/style/DartStylesheet.java
+++ bundles/at.bestsolution.dart.editor/src/at/bestsolution/dart/editor/style/DartStylesheet.java
@@ -1,10 +1,24 @@
 package at.bestsolution.dart.editor.style;
 
+import java.net.URL;
+
 import org.eclipse.fx.code.editor.fx.themes.EditorStylesheet;
 import org.eclipse.fx.ui.services.theme.Stylesheet;
+import org.eclipse.fx.ui.services.theme.Theme;
+import org.eclipse.jdt.annotation.Nullable;
 import org.osgi.service.component.annotations.Component;
 
-@Component(service=Stylesheet.class)
-public class DartStylesheet extends EditorStylesheet {
+@Component
+public class DartStylesheet implements Stylesheet {
+
+	@Override
+	public boolean appliesToTheme(Theme t) {
+		return true;
+	}
+
+	@Override
+	public @Nullable URL getURL(Theme t) {
+		return getClass().getClassLoader().getResource("css/highlight.css");
+	}
 
 }
